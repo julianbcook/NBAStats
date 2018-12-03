c6149b440d250660a3097866dfc78d26b80e04f1
JoseH1497 <jherrera1497@gmail.com>
2018-12-03 15:11:14 -0800

    original master

--- NBAStats/NBAStats/wsgi.py
+++ NBAStats/NBAStats/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for NBAstats project.
+WSGI config for NBAStats project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NBAstats.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NBAStats.settings")
 
 application = get_wsgi_application()
--- NBAStats/Stats/views.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.shortcuts import render, HttpResponse
-
-def index(request):
-    return render(request, 'Stats/index.html')
-# Create your views here.
