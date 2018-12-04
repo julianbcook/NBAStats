e797a15cfbe2eca4c503475336ac1759add2a83f
JoseH1497 <jherrera1497@gmail.com>
2018-12-03 22:39:31 -0800

    Players added to tables

--- Stats/urls.py
+++ Stats/urls.py
@@ -3,5 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-               url(r'^', views.index, name='index'),
+               url(r'^$', views.index, name='index'),
+               url(r'^i', views.index, name='index'),
+               url(r'^l', views.login, name = 'login'),
+               url(r'^c',views.charts, name = 'charts'),
+               url(r'^t',views.tables, name = 'tables'),
+               url(r'^r',views.register, name = 'register'),
+               url(r'^f',views.forgot, name = 'forgotPassword'),
+               url(r'^4',views.badPage, name = '404Page'),
+               url(r'^b',views.blank, name = 'blank'),
 ]
--- Stats/views.py
+++ Stats/views.py
@@ -1,7 +1,37 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render, HttpResponse
+from .models import Player
 
 def index(request):
     return render(request, 'Stats/index.html')
+    #return HttpResponse(x)
 # Create your views here.
+def login(request):
+    return render(request,'Stats/login.html')
+
+def register(request):
+    return render(request,'Stats/register.html')
+
+def badPage(request):
+    return render(request,'Stats/404.html')
+
+def blank(request):
+    return render(request,'Stats/blank.html')
+
+def charts(request):
+    return render(request,'Stats/charts.html')
+
+def forgot(request):
+    return render(request,'Stats/forgot-password.html')
+
+def tables(request):
+    all_players = Player.objects.all()
+    query = "SELECT 1 id, p_pname, p_age, p_points, p_assists, p_fgp, p_3pp FROM PLAYER"
+    q = []
+    
+    for p in Player.objects.raw(query):
+        q.append(p)
+
+    args = {'playerList' : q}
+    return render(request,'Stats/tables.html', args)
